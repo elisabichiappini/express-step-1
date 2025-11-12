f731dcb7ca2a71b88a128edd888c67f9c84b89e6
Elisa Chiappini <elisabichiappini@gmail.com>
2025-11-12 19:14:46 +0100

    feat: express server

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const express = require('express');
 //inizializziamo express come server
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('helloworld');
-});
+
+//importa il router delle pizze
+const pizze = require('./routers/pizze.js');
+
+//usa il router
+app.use('/pizze', pizze,); //quindi andare su http://localhost:3000 e aggiungere /pizze
+
+
 app.listen(port, () => {
     console.log(`App listening port http://localhost:${port}`);
     //la porta di default di express è 3000
---
+++ routers/pizze.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+ 
+//index
+router.get('/', (req, res) => {
+    res.send('lista delle pizze')
+});
+
+//show
+router.get('/:id', (req, res) => {
+    res.send('pizza specifica' + req.params.id)
+});
+
+//store
+router.post('/', (req, res) => {
+    res.send('creazione nuova pizza');
+})
+
+module.exports = router;
